d914bd61bb199cd4de60da2b3a73cd25f0474cf6
hazielxp77 <50523129+hazielxp77@users.noreply.github.com>
2021-01-25 18:12:57 -0300

    Add files via upload

---
+++ Sql/usuarios.sql
@@ -0,0 +1,7 @@
+Create table usuarios (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+Seu nome Varchar(40),
+Seu Email Varchar(40),
+Assunto Varchar(40)
+Menssagen Varchar(40)
+Primary Key (ID)) ENGINE = MyISAM;
